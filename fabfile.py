5f0937f51b6626f7f64fda66d9b1c7ecf811c551
Omar Hernández Padrón <omaherpad@gmail.com>
2018-01-29 12:18:20 +0000

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import env, cd, local, run
+
+# nombre de la máquina de producción
+env.hosts = ["cloud"]
+# env.user
+# env.password
+
+
+def deploy():
+    local("git push")
+    with cd("~/Mkdocs"):
+        run("git pull")
+        run("mkdocs build")
+
